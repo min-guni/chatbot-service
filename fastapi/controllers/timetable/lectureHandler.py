8200aab777f1ec25aac809158c0ceca58db8abdf
Gwanwoo <sgw1213@yonsei.ac.kr>
2024-02-15 14:52:12 +0900

    search lecture (korean, english,  video/blended

--- fastapi/controllers/timetable/lectureHandler.py
+++ fastapi/controllers/timetable/lectureHandler.py
@@ -32,7 +32,120 @@ def search_lecture(q: str):  # 강의 탐색
     print(lecture_list)
     # lecture list에 담아서 넘기기
     return lecture_list
+def search_lecture_english(q: str):  # 강의 탐색
+    query = { 
+        "bool" : {
+            "must": 
+            [ 
+            {
+                "match" : {
+                    "caution" : "영어강의"
+                }
+            },
+            
+            {
+                "multi_match" : {
+                    "query":    query_text, 
+                    "fields": [ "course_name^2", "course_name.course_keyword^2", "course_name.course_english", "course_desc", "course_desc.desc_english" ,"schedule", "instructor"] 
+            }
+            }
+            ]
+        }
+    }
+    fields = ["course_name", "course_code", "schedule", "instructor", "campus", "caution", "classroom",
+              "grade", "major", "semester", "subject_type"]
+    index = 'course_final'
+    # query parameter
+    resp = es.search(index=index,
+                     query=query,
+                     fields=fields,
+                     size=10,
+                     source=False)
 
+    tops = resp['hits']['hits'][:10]
+
+    lecture_list = [{key: value[0] for key, value in d["fields"].items()} for d in tops]
+    print(lecture_list)
+    # lecture list에 담아서 넘기기
+    return lecture_list
+
+def search_lecture_video_blended(q: str):  # 강의 탐색
+    query = { 
+        "bool" : {
+            
+            "must": 
+            [{
+                "multi_match" : {
+                    "query":    query_text, 
+                    "fields": [ "course_name^2", "course_name.course_keyword^2", "course_name.course_english", "course_desc", "course_desc.desc_english" ,"schedule", "instructor"] 
+            }
+            }],
+            "must_not" : [{
+                "match" : {
+                    "caution" : " 대면강의"
+                },
+                "match" : {
+                    "caution" : "대면강의"
+                }
+            }]
+            
+        }
+    }
+    fields = ["course_name", "course_code", "schedule", "instructor", "campus", "caution", "classroom",
+              "grade", "major", "semester", "subject_type"]
+    index = 'course_final'
+    # query parameter
+    resp = es.search(index=index,
+                     query=query,
+                     fields=fields,
+                     size=10,
+                     source=False)
+
+    tops = resp['hits']['hits'][:10]
+
+    lecture_list = [{key: value[0] for key, value in d["fields"].items()} for d in tops]
+    print(lecture_list)
+    # lecture list에 담아서 넘기기
+    return lecture_list
+
+def search_lecture_korean(q: str):  # 강의 탐색
+    query = { 
+        "bool" : {
+            "must": 
+            [
+            {
+                "multi_match" : {
+                    "query":    query_text, 
+                    "fields": [ "course_name^2", "course_name.course_keyword^2", "course_name.course_english", "course_desc", "course_desc.desc_english" ,"schedule", "instructor"] 
+            }
+            }
+            ],
+            "must_not":
+            [
+                {
+                    "match" : {
+                    "caution" : "영어강의"
+                }
+                }
+            ]
+        }
+    }
+    fields = ["course_name", "course_code", "schedule", "instructor", "campus", "caution", "classroom",
+              "grade", "major", "semester", "subject_type"]
+    index = 'course_final'
+    # query parameter
+    resp = es.search(index=index,
+                     query=query,
+                     fields=fields,
+                     size=10,
+                     source=False)
+
+    tops = resp['hits']['hits'][:10]
+
+    lecture_list = [{key: value[0] for key, value in d["fields"].items()} for d in tops]
+    print(lecture_list)
+    # lecture list에 담아서 넘기기
+    return lecture_list
 
 @router.get("/detail/{id}")
 def get_detail(id: str):
